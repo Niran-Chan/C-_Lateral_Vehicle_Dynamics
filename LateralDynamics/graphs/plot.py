2371bd5a7f235967161713ac1b0800223a1e705e
Niran_Chan <niranshivu@gmail.com>
2023-12-18 23:01:13 +0800

    Python plot script can be used without accessing code

--- LateralDynamics/graphs/plot.py
+++ LateralDynamics/graphs/plot.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import sys
 
-df = pd.read_csv('data1.csv')
-df.drop(df.filter(regex="Unname"),axis=1, inplace=True) #Delete Unnamed Cols unknowingly passed from cpp
+try:
+    
+    df = pd.read_csv(sys.argv[1])
+    #df.drop(df.filter(regex="Unname"),axis=1, inplace=True) #Delete Unnamed Cols unknowingly passed from cpp
 
-df.plot(x="Time",subplots=True) #Plot all subplots w.r.t Time
+    #df.plot(x="Time",subplots=True) #Plot all subplots w.r.t Time
 
-plt.show() 
+    #print(df)
+    plt.plot(df)
+    
 
+    plt.show() 
+except:
+    print("[-] Error. Run in the following format\ne.g.\tpython3 ./plot.py ../its/in/here/simulate.csv")
