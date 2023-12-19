688dbb28a73dadce70a9aebce1aa5fa187c11bd5
Niran_Chan <niranshivu@gmail.com>
2023-12-19 17:46:28 +0800

    Clean up NaNs & Axis Labels

--- LateralDynamics/graphs/plot.py
+++ LateralDynamics/graphs/plot.py
@@ -5,15 +5,14 @@ import sys
 
 try:
     
-    df = pd.read_csv(sys.argv[1])
+    df = pd.read_csv(sys.argv[1],verbose=True,skip_blank_lines=True)
     #df.drop(df.filter(regex="Unname"),axis=1, inplace=True) #Delete Unnamed Cols unknowingly passed from cpp
-
-    #df.plot(x="Time",subplots=True) #Plot all subplots w.r.t Time
-
-    #print(df)
-    plt.plot(df)
     
+    #Values are read as each column being a new step in time,meaning that each row is the header
+    df_cleaned = list(filter( lambda x: "nan" not in x , df.columns )) #Remove NaN Values
+    plt.plot(np.asarray(df_cleaned,float))
 
     plt.show() 
-except:
-    print("[-] Error. Run in the following format\ne.g.\tpython3 ./plot.py ../its/in/here/simulate.csv")
+except Exception as e:
+    print("[-] Error. Try running in the following format\ne.g.\tpython3 ./plot.py ../its/in/here/simulate.csv")
+    print("[-] ",e)
