22fb9075942ae0bbf00eccadfc2fc81a10ff3667
Niran_Chan <niranshivu@gmail.com>
2023-12-31 10:33:34 +0530

    Added specific vehicle params

--- LateralDynamics/Vehicle.cpp
+++ LateralDynamics/Vehicle.cpp
@@ -24,23 +24,28 @@ Vehicle::Vehicle(){
     double m   = ego.param[:m] //Mass
     double Iz  = ego.param[:Iz] //Mass Moment of Inertia About Z Axis
      */
-    this -> v = 1; //Velocity, m/s
+    this -> v = 0.0; //Velocity, m/s
     this -> δf = M_PI_4; //Angle of Front Tire wrt longitudinal axis of Vehicle (rad)
-    this -> δr = 0; //0 as vehicle is front wheel steering only
-    this -> lf  = 3.5;
-    this -> lr  = 3.5;
+    this -> δr = 0.0; //0 as vehicle is front wheel steering only
+    this -> lf  = 1.803;
+    this -> lr  = 1.517;
     this -> m   = 1000; //Mass in kg
-    this -> Iz  = (1.0/12.0) *(std::pow(2,2) + std::pow(2.5,2));//Mass Moment of Inertia About Z Axis (Approximation of 6m length, 2m width and 2.5m height)
+    //this -> Iz  = (1.0/12.0) *(std::pow(2,2) + std::pow(2.5,2));//Mass Moment of Inertia About Z Axis (Approximation of 6m length, 2m width and 2.5m height)
+    this -> Iz = 9620.0;
     //ENSURE FOR FLOAT MATH OPERATIONS, REPRESENT NUMBERS WITH DECIMALS
-    this -> ψ = M_PI_4; //heading angle: Orientation of vehicle wrt global X axis in radians
+    this -> ψ = -2.66446209; //heading angle: Orientation of vehicle wrt global X axis in radians
     
     //Slip Dynamics Coefficients
-    this -> Cαf = 1;//Proportionality Constant, in this case Cornering Stiffness of Front Wheel
-    this -> Cαr = 1;//Proportionality Constant, in this case Cornering Stiffness of Rear Wheel
+    this -> Cαf = 160830.0;//Proportionality Constant, in this case Cornering Stiffness of Front Wheel
+    this -> Cαr = 160320.7;//Proportionality Constant, in this case Cornering Stiffness of Rear Wheel
     this -> θVf = 1; //Front Tyre Velocity Angle (A portion of the δf as at high speeds, velocity does not immediately change due to inertia) (rad)
     this -> θVr = 1;
     this -> lw = 2.5; //car wheelbase width
-    this -> L = 7; //Length of Vehicle
+    this -> L = lr+lf; //Length of Vehicle
+    //Include Steering Angle (Maximum and current? ) 
+    
+    
+    
     //State space model (excluding road bank angle)
     //u is the final output
     //std::vector<std::vector<std::vector<double>>> du; //Dynamics Model
