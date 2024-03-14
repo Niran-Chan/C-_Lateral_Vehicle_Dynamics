e600dc320d0233762c0bf0705ee2bb9b0bc595b3
Niran_Chan <niranshivu@gmail.com>
2024-03-14 19:57:40 +0800

    Control Block Implementation with TransferFunction

--- LateralDynamics/ControlBlock.cpp
+++ LateralDynamics/ControlBlock.cpp
@@ -5,4 +5,19 @@
 //  Created by Niranjan Gopinath on 14/3/24.
 //
 
-#include "ControlBlock.hpp"
+#include "ControlBlock.hpp""
+
+ControlBlock::ControlBlock(){
+    this -> nextBlock = NULL;
+}
+ControlBlock::ControlBlock(Polynomial* num,Polynomial* denom){
+    this -> tf.numPoly = num;
+    this -> tf.denomPoly = denom;
+    this -> nextBlock = NULL;
+}
+ControlBlock::ControlBlock(Polynomial* num,Polynomial* denom,ControlBlock* nextBlock){
+    this -> tf.numPoly = num;
+    this -> tf.denomPoly = denom;
+    this -> nextBlock = nextBlock;
+}
+
--- LateralDynamics/ControlBlock.hpp
+++ LateralDynamics/ControlBlock.hpp
@@ -9,5 +9,44 @@
 #define ControlBlock_hpp
 
 #include <stdio.h>
+#include <string>
+#include <unordered_map>
+#include <stack>
+#include <fstream>
+#include <iostream>
+#include "Polynomial.hpp"
+
+class ControlBlock{
+public:
+    
+    //DataType TransferFunction
+    
+    struct TransferFunction{
+        Polynomial* numPoly;
+        Polynomial* denomPoly;
+    } tf; //Transferfunction of type TransferFunction
+    
+    ControlBlock* nextBlock; //Linked List Methodology
+    
+    
+    
+    /*!
+            Default Constructor. Initialises Empty Instance
+     */
+    ControlBlock();
+    /*!
+                Overloaded Constructor. Initialise with Transfer Function
+     \param tf Transfer function in s-domain
+     */
+    ControlBlock(Polynomial* num,Polynomial* denom);
+    /*!
+                Overloaded Constructor
+        \param tf Transfer function in s-domain
+        \param nextBlock Another ControlBlock instance as the next block to continue to.
+     */
+//Overloaded Constructor
+    ControlBlock(Polynomial* num, Polynomial* denom, ControlBlock* nextBlock);
+};
+
 
 #endif /* ControlBlock_hpp */
